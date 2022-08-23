78c694b58112d34c60207a28bddbbb6e3be38e24
Oleksandr Horiainov <oleksandr.horiainov@checkr.com>
2022-08-23 16:37:26 +0300

    Use context for sharing state

--- src/App.js
+++ src/App.js
@@ -1,42 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Switch, Route, Link } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { About } from './pages/About';
-import { Articles } from './pages/Articles';
+import { CounterButton } from './CounterButton';
+import { CounterProvider } from './CounterProvider';
 
-const BigGreenHeading = styled.h1`
-  color: green;
-  font-size: 96px;
-`;
 const App = () => {
 	return (
-		<>
-    <BigGreenHeading>Server-Side Rendering Example</BigGreenHeading>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/articles">Articles</Link>
-      </li>
-    </ul>
-    <Switch>
-      <Route path="/" exact>
-        <Home />
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/articles">
-        <Articles />
-      </Route>
-    </Switch>
-		</>
-	);
+    <CounterProvider>
+      <h1>State Management Example</h1>
+      <CounterButton />
+    </CounterProvider>
+  );
 }
 
 export default App;
---
+++ src/CounterButton.js
@@ -0,0 +1,16 @@
+import { useContext } from "react";
+import { CounterContext } from "./CounterContext";
+
+export const CounterButton = () => {
+  const { numberOfClicks, increment, incrementBy, setIncrementBy } = useContext(CounterContext);
+  return (
+    <>
+    <p>You have clicked the button {numberOfClicks} times</p>
+    <label>
+      Increment by:
+      <input value={incrementBy} onChange={e => setIncrementBy(Number(e.target.value))} type="number"/>
+    </label>
+    <button onClick={() => increment()}>Click</button>
+    </>
+  );
+}
