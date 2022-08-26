308f617efaf7d24a3be4e21f367d8f7ed150f012
Oleksandr Horiainov <oleksandr.horiainov@checkr.com>
2022-08-26 14:24:16 +0300

    Implement data loadign

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import 'isomorphic-fetch';
 import express from "express";
 import React from "react";
 import { ServerStyleSheet } from "styled-components";
@@ -6,19 +7,49 @@ import { StaticRouter } from "react-router-dom";
 import path from 'path';
 import fs from 'fs';
 import App from "./src/App";
+import { InitialDataContext } from './src/InitialDataContext';
+
+global.window = {};
 
 const app = express();
 
 app.use(express.static('./build', { index: false }));
 
-app.get('/*', (req, res) => {
+const articles = [
+  { title: 'Article 1', author: 'John'},
+  { title: 'Article 2', author: 'Johana'},
+  { title: 'Article 3', author: 'Ivan'},
+];
+
+app.get('/api/articles', (req, res) => {
+  const loadedArticles = articles;
+
+  res.json(loadedArticles);
+});
+
+app.get('/*', async (req, res) => {
   const sheet = new ServerStyleSheet();
-  const reactApp = renderToString(
+  const contextObj = { _isServerSide: true, _requests:[], _data: {} }
+  renderToString(
     sheet.collectStyles(
+      <InitialDataContext.Provider value={contextObj}>
+        <StaticRouter location={req.url}>
+          <App />
+        </StaticRouter>
+      </InitialDataContext.Provider>
+    )
+  );
+
+  await Promise.all(contextObj._requests);
+  contextObj._isServerSide = false;
+  delete contextObj._requests;
+
+  const reactApp = renderToString(
+    <InitialDataContext.Provider value={contextObj}>
       <StaticRouter location={req.url}>
         <App />
       </StaticRouter>
-    )
+    </InitialDataContext.Provider>
   );
 
   const templateFile = path.resolve('./build/index.html')
@@ -28,7 +59,7 @@ app.get('/*', (req, res) => {
     }
 
     return res.send(
-      data.replace('<div id="root"></div>', `<div id="root">${reactApp}</div>`)
+      data.replace('<div id="root"></div>', `<script>window.preloadedData = ${JSON.stringify(contextObj)}</script><div id="root">${reactApp}</div>`)
           .replace('{{ styles }}', sheet.getStyleTags())
     );
   });
